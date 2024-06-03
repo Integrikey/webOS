4116ea67104c1e66732b7593921861978b2e0f21
Juan Martin Garcia <hi@juanmartingarcia.com>
2024-06-03 08:50:18 -0300

    KS-1767: Link demo button to signup page

--- components/wrapper/Sidebar.tsx
+++ components/wrapper/Sidebar.tsx
@@ -52,12 +52,20 @@ export const SidebarCTA: FC<SidebarCTAProps> = ({ isAttackerMode, openModal }) =
     >
       Contact us and get a free 30-day trial, no strings attached!
     </p>
-    <OutlinedButton
-      action={() => openModal()}
-      isAttackerMode={isAttackerMode}
-      text="Request a free trial"
-      isModalTrigger
-    />
+    <a
+      className={`
+      px-4 py-1
+      border border-solid rounded-md
+      transition-all duration-200 ease-in-out
+      border-gray-200 hover:bg-teal-700 hover:border-teal-700 ${isAttackerMode ? 'text-white' : 'text-gray-700 hover:text-white'}
+      text-sm font-medium
+      `}
+      href='https://signup.keystrike.com/'
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Request a free trial
+    </a>
   </div>
 );
 
